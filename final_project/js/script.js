f935b6a5101c1aa4b43294547ac0f44d1e85c1f3
LiamRyan22 <lryan@eastsideprep.org>
2022-02-16 13:31:58 -0800

    commit

---
+++ final_project/js/script.js
@@ -0,0 +1,7 @@
+// w3
+mybutton = document.getElementById("myBtn");
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
